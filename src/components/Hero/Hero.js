248aae654d6afd0baecdd82f2d3c36205ff5538f
Elyes Arifa <arifa.elyes@gmail.com>
2023-02-07 06:04:06 +0100

    change title

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { Img, Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { Img, Section, SectionText, SectionTitle } from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
         Elyes Arifa <br />
-        Software Engineering
+        Software Engineer
       </SectionTitle>
 
       <SectionText>
-        Experienced software engineer with expertise in full stack development, proficient with various frameworks and libraries. Able to work
-        independently on projects, as well as collaborate with as a part of team when needed
+        Experienced software engineer with expertise in full stack development, proficient with various frameworks and
+        libraries. Able to work independently on projects, as well as collaborate with as a part of team when needed
       </SectionText>
 
-      <Button onClick={() => (window.location = 'https://www.upwork.com/freelancers/~01eca81ee658286fea')}>Learn More</Button>
+      <Button onClick={() => (window.location = "https://www.upwork.com/freelancers/~01eca81ee658286fea")}>
+        Learn More
+      </Button>
     </LeftSection>
   </Section>
 );
